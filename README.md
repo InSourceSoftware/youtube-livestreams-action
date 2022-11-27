c32a7be62f3cf01e474b6b8e8c4ef8732fbcc815
Stephen Riesenberg <sjohnr@hotmail.com>
2022-11-27 15:08:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# YouTube Videos Action
+
+Fetch YouTube videos from a YouTube channel (by channel id) and create files or
+posts on a website.
+
+## Inputs
+
+### `api-key`
+
+**Required** Google api key with access to YouTube Data API v3
+
+### `channel-id`
+
+**Required** YouTube channel id
+
+### `max-results`
+
+**Optional** Max number of results to return, defaults to 5
+
+### `thumbnail-size`
+
+**Optional** Thumbnail image size name, one of default, medium, high, standard,
+maxres
+
+### `output-path`
+
+**Optional** Path to write output files to, defaults to `_data/streams`
+
+### `output-filename-template`
+
+**Optional** Output filename template for each output file including
+placeholders, defaults to `${publishedAt}.yml`
+
+### `output-content-template`
+
+**Optional** Output content template for each output file including
+placeholders[1], defaults to:
+
+```
+_id: '${id}'
+etag: '${etag}'
+title: '${title}'
+description: '${description}'
+thumbnailUrl: '${thumbnailUrl}'
+channelId: '${channelId}'
+channelTitle: '${channelTitle}'
+videoId: '${videoId}'
+publishedAt: '${publishedAt}'
+publishTime: '${publishTime}'
+duration: '${duration}'
+actualStartTime: '${actualStartTime}'
+actualEndTime: '${actualEndTime}'
+```
+
+[1]: Placeholders currently include the following fields from the YouTube
+playlist response:
+
+* `${id}`
+* `${etag}`
+* `${title}`
+* `${description}`
+* `${thumbnailUrl}`
+* `${channelId}`
+* `${channelTitle}`
+* `${videoId}`
+* `${publishedAt}`
+* `${publishTime}`
+* `${duration}`
+* `${actualStartTime}`
+* `${actualEndTime}`
+
+## Example usage
+
+```yaml
+uses: InSourceSoftware/youtube-livestreams-action@v1
+with:
+  api-key: ${{ secrets.GOOGLE_API_KEY }}
+  channel-id: 'xyz'
+  thumbnail-size: 'standard'
+  output-path: '_data/videos'
+  output-filename-template: '${position}.yml'
+  output-content-template: |
+    _id: '${id}'
+    etag: '${etag}'
+    title: '${title}'
+    description: '${description}'
+    thumbnailUrl: '${thumbnailUrl}'
+    channelId: '${channelId}'
+    channelTitle: '${channelTitle}'
+    playlistId: '${playlistId}'
+    position: '${position}'
+    videoOwnerChannelTitle: '${videoOwnerChannelTitle}'
+    videoOwnerChannelId: '${videoOwnerChannelId}'
+    videoId: '${videoId}'
+    publishedAt: '${publishedAt}'
+```
---
+++ index.js
@@ -0,0 +1,136 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const request = require('request');
+const fs = require('fs');
+
+const YOUTUBE_URL = 'https://youtube.googleapis.com';
+
+main();
+
+function main() {
+  const apiKey = core.getInput('api-key');
+  console.log('apiKey=***')
+  const channelId = core.getInput('channel-id');
+  console.log(`channel-id=${channelId}`);
+  const maxResults = core.getInput('max-results');
+  console.log(`max-results=${maxResults}`);
+  const thumbnailSize = core.getInput('thumbnail-size');
+  console.log(`thumbnailSize=${thumbnailSize}`);
+  const outputPath = core.getInput('output-path');
+  console.log(`outputPath=${outputPath}`);
+  const outputFilenameTemplate = core.getInput('output-filename-template');
+  console.log(`outputFilenameTemplate=${outputFilenameTemplate}`);
+  const outputContentTemplate = core.getInput('output-content-template');
+  console.log(`outputContentTemplate=${outputContentTemplate}`);
+
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`payload=${payload}`);
+
+  console.log('Fetching videos...');
+  try {
+    fetchVideos(apiKey, channelId, thumbnailSize, outputPath, outputFilenameTemplate, outputContentTemplate)
+      .then(() => console.log('finished'));
+  } catch (error) {
+    console.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+async function fetchVideos(apiKey, channelId, maxResults, thumbnailSize, outputPath, outputFilenameTemplate, outputContentTemplate) {
+  const data = await fetchPage(apiKey, channelId, maxResults, null);
+  console.log(`found ${data.pageInfo.totalResults} total results`);
+  const items = data.items
+    .filter(item => Object.keys(item.snippet.thumbnails).length !== 0);
+  const videoIds = items.map(item => item.id.videoId);
+  const videoDetails = await fetchDetails(apiKey, videoIds);
+
+  for (let index in items) {
+    const item = items[index];
+    const details = videoDetails.items.find(video => video.id === item.id);
+    // Copy over additional or richer details
+    if (details !== undefined) {
+      item.snippet.thumbnails = details.snippet.thumbnails;
+      item.contentDetails = details.contentDetails;
+      item.liveStreamingDetails = details.liveStreamingDetails;
+    }
+    const filename = template(outputFilenameTemplate, item, thumbnailSize);
+    const content = template(outputContentTemplate, item, thumbnailSize);
+    await writeFile(outputPath, filename, content);
+  }
+}
+
+function fetchPage(apiKey, channelId, maxResults, pageToken) {
+  return new Promise((resolve, reject) => {
+    let url = `${YOUTUBE_URL}/youtube/v3/search?key=${apiKey}&channelId=${channelId}&maxResults=${maxResults}&part=snippet&type=video&eventType=completed&order=date`;
+    if (pageToken !== null) {
+      url = `${url}&pageToken=${pageToken}`;
+      console.log(`fetching page ${pageToken} of ${channelId}`);
+    } else {
+      console.log(`fetching first page of ${channelId}`);
+    }
+    const options = {
+      'method': 'GET',
+      'url': url,
+      'headers': {}
+    };
+    request(options, (error, response) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(response.body));
+    });
+  });
+}
+
+function fetchDetails(apiKey, videoIds) {
+  return new Promise((resolve, reject) => {
+    const url = `${YOUTUBE_URL}/youtube/v3/videos?key=${apiKey}&id=${videoIds.join(',')}&part=snippet,contentDetails,liveStreamingDetails`;
+    const options = {
+      'method': 'GET',
+      'url': url,
+      'headers': {}
+    };
+    request(options, (error, response) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(response.body));
+    });
+  });
+}
+
+function writeFile(outputPath, filename, content) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(outputPath, { recursive: true }, (error) => {
+      if (error) {
+        reject(error);
+      }
+      fs.writeFile(`${outputPath}/${filename}`, content, { encoding: 'utf-8' }, resolve);
+    });
+  });
+}
+
+function template(str, item, thumbnailSize) {
+  const thumbnail = item.snippet.thumbnails[thumbnailSize] || item.snippet.thumbnails['default'];
+  return str
+    .replace('${id}', item.id.videoId)
+    .replace('${etag}', item.etag)
+    .replace('${title}', clean(item.snippet.title))
+    .replace('${description}', clean(item.snippet.description))
+    .replace('${thumbnailUrl}', thumbnail.url)
+    .replace('${channelId}', item.snippet.channelId)
+    .replace('${channelTitle}', clean(item.snippet.channelTitle))
+    .replace('${videoId}', item.id.videoId)
+    .replace('${publishedAt}', item.snippet.publishedAt)
+    .replace('${publishTime}', item.snippet.publishTime)
+    .replace('${duration}', item.contentDetails.duration)
+    .replace('${actualStartTime}', item.liveStreamingDetails.actualStartTime)
+    .replace('${actualEndTime}', item.liveStreamingDetails.actualEndTime);
+}
+
+function clean(str) {
+  return str
+    .replaceAll('\n', '\\n')
+    .replaceAll('\'', '\'\'')
+    .replaceAll('"', '\\"');
+}
